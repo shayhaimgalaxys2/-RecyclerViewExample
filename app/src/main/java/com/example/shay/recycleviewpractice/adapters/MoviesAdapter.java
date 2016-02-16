4be2c535e604aa66c37a185a265c89eea934748b
shayhaimgalaxys2 <shayhaimgalaxys2@gmail.com>
2016-02-16 22:21:18 +0200

    added all files

---
+++ app/src/main/java/com/example/shay/recycleviewpractice/adapters/MoviesAdapter.java
@@ -0,0 +1,61 @@
+package com.example.shay.recycleviewpractice.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.shay.recycleviewpractice.R;
+import com.example.shay.recycleviewpractice.data_objects.Movie;
+
+import java.util.List;
+
+/**
+ * Created by shay on 16/02/2016.
+ */
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+
+    private List<Movie> movies;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            year = (TextView) view.findViewById(R.id.year);
+            genre = (TextView) view.findViewById(R.id.genre);
+        }
+    }
+
+    public MoviesAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesAdapter.MyViewHolder holder, int position) {
+
+        Movie movie = movies.get(position);
+        holder.title.(setTextmovie.getTitle());
+        holder.year.setText(movie.getYear());
+        holder.genre.setText(movie.getGenre());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
